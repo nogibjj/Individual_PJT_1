48e458562aab812685424867857984673b722482
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-17 19:38:13 +0000

    update files

---
+++ test_lib.py
@@ -0,0 +1,12 @@
+from lib import read
+import pandas as pd
+
+def test_read():
+    file = "Melbourne_housing.csv"
+    df = read(file)
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
+
+if __name__ == "__main__":
+    test_read()
+    
